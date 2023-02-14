6e30c6a7e1cdd99afe7fcd579488b2ddc8fa05cf
Jakob <jakob.wurst@htlstp.at>
2023-02-14 08:59:21 +0100

    Inital Comit

---
+++ src/main/java/at/ac/htlstp/demo/javasj234a/JavaSj234AApplication.java
@@ -0,0 +1,12 @@
+package at.ac.htlstp.demo.javasj234a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSj234AApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSj234AApplication.class, args);
+    }
+}
---
+++ src/test/java/at/ac/htlstp/demo/javasj234a/JavaSj234AApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.demo.javasj234a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSj234AApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
